b958d0e3f17c6ed13acdf91592a8ce549174eb6f
Vinh-Dieu Lam <vinh@wayve.ai>
2020-04-26 14:44:54 +0100

    playing with python

---
+++ python_games/times/times.py
@@ -0,0 +1,13 @@
+questions = [
+    'What is the capital of France',
+    'What is 56 / 7 ?',
+    'What colour is the sky?',
+]
+
+answers = [
+    'Paris',
+    '8',
+    'blue',
+]
+
+input(questions[0])
